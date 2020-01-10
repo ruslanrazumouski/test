df6f47e37c928d3439fd4737922240d00e28a0d1
Ruslan Razumouski <ruslanrazumouski@gmail.com>
2020-01-10 15:37:23 +0100

    commit test class

---
+++ src/examples/Main.java
@@ -0,0 +1,32 @@
+package examples;
+
+class Cat {
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+/*
+ * Java. Examples from book Guskova
+ *
+ * @author Ruslan Razumouski
+ * @version Jan 10, 2020
+ */
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 2);
+        System.out.println(cat);
+    }
+}
