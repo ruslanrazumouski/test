34a5e9ef5712c217adb8f74196b1b5345be2bd4c
Ruslan Razumouski <ruslanrazumouski@gmail.com>
2020-01-11 20:21:23 +0100

    добавили строку -hello, Git-

--- src/examples/Main.java
+++ src/examples/Main.java
@@ -28,5 +28,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
         System.out.println("Ok2");
+        System.out.println("Hello, Git!");
     }
 }
