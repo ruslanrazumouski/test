aa5171607314c122cb98dd0329a0f090dc26d789
Ruslan Razumouski <ruslanrazumouski@gmail.com>
2020-01-12 13:48:22 +0100

    Добавлена строка в класс Second
    add hello git cherry-pick
    
    add hello squash in Second

--- src/examples/Second.java
+++ src/examples/Second.java
@@ -1,4 +1,9 @@
 package examples;
 
 public class Second {
+    public static void main(String[] args) {
+        System.out.println("add println");
+        System.out.println("hello git cherry-pick!");
+        System.out.println("hello squash!");
+    }
 }
