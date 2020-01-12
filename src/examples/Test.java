38c7b66e09d09ab606fe1d14535c2b0e4b081432
Ruslan Razumouski <ruslanrazumouski@gmail.com>
2020-01-12 14:05:31 +0100

    add new hello git rebase master

--- src/examples/Test.java
+++ src/examples/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, Test!");
         System.out.println("add println");
+        System.out.println("Hello, git rebase");
     }
 }
